6bbbf7c5486a7ebe38809130796a99e0f9545bc6
raeygetahun <getahunraey@gmail.com>
2025-04-29 16:22:30 +0200

    updated services

--- components/ServicesWhite.tsx
+++ components/ServicesWhite.tsx
@@ -1,9 +1,12 @@
-'use client';
+"use client";
 import React, { useRef } from "react";
 import { BsGlobe } from "react-icons/bs";
 import { SlScreenSmartphone } from "react-icons/sl";
 import { AiOutlineSecurityScan } from "react-icons/ai";
 import { HiOutlineArrowSmRight } from "react-icons/hi";
+import { AiOutlineRobot } from "react-icons/ai";
+import { RiLightbulbFlashLine } from "react-icons/ri";
+import { FaRobot } from "react-icons/fa"; // Updated import
 
 const ServicesWhite = () => {
   const ref = useRef(null);
@@ -19,9 +22,17 @@ const ServicesWhite = () => {
       }}
     >
       <div className="flex flex-col lg:flex-row justify-around items-center w-full space-y-8 lg:space-y-0 lg:space-x-8">
-        
         {/* FIRST COLUMN */}
         <div className="flex flex-col gap-y-8 w-full sm:w-3/4 md:w-2/3 lg:w-1/3 items-center">
+          <div className="service p-6 md:p-8 w-full bg-white rounded-2xl border border-gray-300 backdrop-blur-md shadow-lg">
+            <div className="flex justify-between items-center">
+              <FaRobot className="text-5xl text-[#11112B]" />
+            </div>
+            <div className="flex flex-col text-2xl font-semibold text-[#11112B]">
+              <span>Artificial</span>
+              <span>Intelligence</span>
+            </div>
+          </div>
           <div className="service p-6 md:p-8 w-full bg-white rounded-2xl border border-gray-300 backdrop-blur-md shadow-lg">
             <div className="flex justify-between items-center">
               <BsGlobe className="text-5xl text-[#11112B]" />
@@ -58,6 +69,14 @@ const ServicesWhite = () => {
         <div className="flex flex-col gap-y-8 w-full sm:w-3/4 md:w-2/3 lg:w-1/3 items-center">
           <div className="service p-6 md:p-8 w-full bg-white rounded-2xl border border-gray-300 backdrop-blur-md shadow-lg">
             <div className="flex justify-between items-center">
+              <RiLightbulbFlashLine className="text-5xl text-[#11112B]" />
+            </div>
+            <div className="flex flex-col text-2xl font-semibold text-[#11112B]">
+              <span>AI Consulting</span>
+            </div>
+          </div>
+          <div className="service p-6 md:p-8 w-full bg-white rounded-2xl border border-gray-300 backdrop-blur-md shadow-lg">
+            <div className="flex justify-between items-center">
               <AiOutlineSecurityScan className="text-5xl text-[#11112B]" />
             </div>
             <div className="flex flex-col text-2xl font-semibold text-[#11112B]">
@@ -78,7 +97,8 @@ const ServicesWhite = () => {
           <div className="text-center px-6 md:px-0">
             <p className="text-4xl font-bold text-[#11112B]">Services</p>
             <p className="text-[#11112B] font-medium text-lg md:text-xl mt-2">
-              We provide customized solutions to meet our clients' unique needs and help them succeed in an increasingly digital world.
+              We provide customized solutions to meet our clients' unique needs
+              and help them succeed in an increasingly digital world.
             </p>
             {/* <button className="bg-[#11112B] text-white px-6 py-3 rounded-lg mt-4 text-lg font-semibold hover:bg-[#22224A] transition duration-300">
               Learn More
